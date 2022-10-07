2bb88de18eacd59fec8332e734f430cbd521c46f
Kevin Liu <kliu@southtexascollege.edu>
2022-10-07 10:02:09 -0500

    basic validation

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,29 @@
 <script setup>
-  import { ref } from 'vue'
-  const title = ref('Hello There ✋')
+  import { onMounted } from 'vue'
+  import BaseCard from '@/components/BaseCard.vue'
+  import useCharacters from '@/composables/useCharacters'
+
+  const { characters, fetchCharacters, firstLoad } = useCharacters()
+
+  onMounted(async () => {
+    if (firstLoad.value) {
+      await fetchCharacters()
+      firstLoad.value = false
+    }
+  })
 </script>
 
 <template>
-  <main class="flex min-h-screen items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
+  <button
+    title="Load Characters"
+    class="z-90 fixed bottom-8 right-8 flex h-20 w-20 items-center justify-center rounded-full bg-green-600 text-4xl text-white drop-shadow-lg duration-300 hover:scale-110 hover:bg-green-700 hover:drop-shadow-2xl"
+    @click="fetchCharacters"
+  >
+    🚀
+  </button>
+  <main class="min-h-screen bg-gradient-to-r from-fuchsia-900 to-red-700">
+    <div class="container mx-auto grid grid-cols-8 gap-4 py-8">
+      <BaseCard v-for="character in characters" :key="character._id" :character="character" />
+    </div>
   </main>
 </template>
